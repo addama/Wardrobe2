8ec0cc9a4d7735ed11f10b386b00ff0a4eb34ce5
Addama <addama.sanders@gmail.com>
2018-06-29 14:47:30 -0700

    Got the Rating object and child objects sorted out. Rating is now an abstract class, with the usable classes differing only by the array of strings they represent.

--- Wardrobe2/Rating.cs
+++ Wardrobe2/Rating.cs
@@ -2,33 +2,56 @@
 using System.Collections.Generic;
 
 namespace Wardrobe2 {
-	class Rating {
-		private int current;
-		private int max;
-		private static int defaultMax = 5;
 
-		public Rating(int current, int max = -1) {
-			if (current < 1) current = 1;
-			if (current > defaultMax) current = defaultMax;
-			if (max == -1) max = defaultMax;
-			this.current = current;
-			this.max = max;
+	abstract class Rating {
+		protected int current = 0;
+		protected int max = 5;
+		protected string[] ratings;
+
+		public Rating(int current, string[] ratings) {
+			this.ratings = ratings;
+			this.max = this.ratings.Length - 1;
+			if (current < 0) current = 0;
+			if (current > this.max) current = this.max;
 		}
 
-		public Rating(string current, int max = -1) : this(Int32.Parse(current), max) {}
+		public string getRating() {
+			return this.ratings[this.current];
+		}
 
-		public int getRating() {
+		public int getIndex() {
 			return this.current;
 		}
 
+		public int getMax() {
+			return this.max;
+		}
+
 		public void setRating(int value) {
-			if (value < 1) value = 1;
-			if (value > defaultMax) value = defaultMax;
+			if (value < 0) value = 0;
+			if (value > this.max) value = this.max;
 			this.current = value;
 		}
 
-		public void setRating(string value) {
-			this.setRating(Int32.Parse(value));
-		}
+	}
+
+	class GoodBadRating : Rating {
+		public GoodBadRating(int current) : base(current, new string[] { "Terrible", "Bad", "Neutral", "Good", "Great" }) {}
+	}
+
+	class FormalityRating : Rating {
+		public FormalityRating(int current) : base(current, new string[] { "Casual", "Semi-Casual", "Business", "Semi-Formal", "Formal" }) {}
+	}
+
+	class WarmthRating : Rating {
+		public WarmthRating(int current) : base(current, new string[] { "Cool", "Neutral", "Warm" }) { }
+	}
+
+	class WearRating : Rating {
+		public WearRating(int current) : base(current, new string[] { "Ratty", "Well-worn", "Like New", "Pristine" }) { }
+	}
+
+	class FitRating : Rating {
+		public FitRating(int current) : base(current, new string[] { "Too tight", "Tight", "Fits", "Loose", "Too loose" }) { }
 	}
 }
